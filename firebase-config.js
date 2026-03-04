1e1226c015af2faf581a137febd92d0223dc9a8d
Azaharul Islam <Azaharulislam171@gmail.com>
2026-03-04 10:01:16 -0500

    Move secrets to firebase-env.js and add gitignore

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import firebaseEnv from "./firebase-env.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB2pAdTz4x8fbf_Jpg5yuLGoVa_Srx7I3M",
-  authDomain: "learningfirebase-521f7.firebaseapp.com",
-  projectId: "learningfirebase-521f7",
-  storageBucket: "learningfirebase-521f7.firebasestorage.app",
-  messagingSenderId: "577707917241",
-  appId: "1:577707917241:web:9a8641bb12b13e57b8c068"
-};
-
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseEnv);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
